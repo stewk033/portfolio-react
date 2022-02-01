8c825abeaa23f16f1a0fa8882c39ec18c4cf9268
stewk033 <kevin.c.stewart04@gmail.com>
2022-02-01 10:51:18 -0500

    update portfolio/deploy app

--- src/data/portfolioArray.js
+++ src/data/portfolioArray.js
@@ -1,22 +1,58 @@
 import imgHeroverse from '../images/heroverselogo.png';
-import imgPlagueDocs from '../images/plaguedocslogo.png'
+import imgPlagueDocs from '../images/plaguedocslogo.png';
+import imgWeatherDashboard from '../images/weatherdashboard.png';
+import imgGymBuddy from '../images/gymbuddy.jpeg';
+import imgNoteTaker from '../images/notetaker.png';
+import imgMarioKart from '../images/mario.png';
 
 export const portfolioArray = [
     {
-      id: '',
-      url: 'https://github.com/stewk033/heroverse',
-      title: 'HeroVerse',
-      imgSrc: imgHeroverse,
-      alt: 'heroverse logo',
-      languages: 'Bootstrap | JQuery | localStorage',
+        id: '',
+        url: 'https://github.com/stewk033/heroverse',
+        title: 'HeroVerse',
+        imgSrc: imgHeroverse,
+        alt: 'heroverse logo',
+        languages: 'Bootstrap | JQuery | localStorage',
     },
     {
-      id: '',
-      url: 'https://github.com/stewk033/plague-docs',
-      title: 'Plague Docs',
-      imgSrc: imgPlagueDocs,
-      alt: 'Plague Docs logo',
-      languages: 'React | JavaScript | APIs',
+        id: '',
+        url: 'https://github.com/stewk033/plague-docs',
+        title: 'Plague Docs',
+        imgSrc: imgPlagueDocs,
+        alt: 'Plague Docs logo',
+        languages: 'React | JavaScript | APIs',
+    },
+    {
+        id: '',
+        url: 'https://github.com/stewk033/weather-dashboard',
+        title: 'Weather Dashboard',
+        imgSrc: imgWeatherDashboard,
+        alt: 'Weather Dashboard screenshot',
+        languages: 'HTML | CSS | APIs',
+    },
+    {
+        id: '',
+        url: 'https://github.com/stewk033/gym-buddy',
+        title: 'Gym Buddy',
+        imgSrc: imgGymBuddy,
+        alt: 'Gym Buddies',
+        languages: 'HTML | CSS',
+    },
+    {
+        id: '',
+        url: 'https://peaceful-everglades-63833.herokuapp.com/',
+        title: 'Note Taker',
+        imgSrc: imgNoteTaker,
+        alt: 'Note Taker screenshot',
+        languages: 'EXPRESS',
+    },
+    {
+        id: '',
+        url: 'https://stewk033.github.io/mario-kart-randomizer/',
+        title: 'Mario Kart Randomizer',
+        imgSrc: imgMarioKart,
+        alt: 'Mario',
+        languages: 'HTML | JavaScript | APIs',
     },
   
   ];
